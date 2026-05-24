54835bfbfb4d32d451fb5c47e988a765fc4f2290
abhinavdev17 <abhinavdeveloper17@gmail.com>
2026-05-24 12:43:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+
+export default function App() {
+  const [Input, setInput] = useState("")
+  const nameref=useRef()
+  const passref=useRef()
+
+const name ="admin"
+const password="Admin@1"
+
+function handleSbumit(){
+
+if(nameref.current.value==name && passref.current.value== password){
+  alert("login successfull")
+}
+else{
+alert("invalid credentials")
+}
+}
+
+
+
+  return (
+    <div>
+<form action="" onSubmit={handleSbumit} >
+  <input type="text" ref={nameref} placeholder='enter your username' />
+  <input type="password" ref={passref} placeholder='enter your password' />
+  <button type='submit' >submit</button>
+  
+</form>
+
+
+    </div>
+  )
+}
